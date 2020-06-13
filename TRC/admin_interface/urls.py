e437727e03beeda7b95df3c297755f4780fc65c8
Dottore <patryk.czemierowski@gmail.com>
2020-06-13 15:14:50 +0200

    home page + RWD+ landning admin

--- TRC/admin_interface/urls.py
+++ TRC/admin_interface/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [path('add',views.add_page,name='add_page'),
                path('stats',views.stats_page, name = 'stats_page'),
                path('all_products', views.all_products,name='all_products'),
-               path('orders_list',views.orders_list,name='orders_list_page'),
+               path('orders_list',views.orders_list,name='shop_orders'),
                path('edit_pages',views.edit_pages,name ='edit_pages'),
                path('edit_data',views.edit_data,name='edit_data'),
                path('create', views.new_product, name='new_product'),
